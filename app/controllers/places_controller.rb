66b04edb443355aa0d2cae0e046f34f6c22a5948
Kareem <kareem_rady@yahoo.com>
2015-11-29 04:07:38 +0000

    Addede name validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -7,8 +7,12 @@ class PlacesController < ApplicationController
 		@place = Place.new
 	end
 	def create
-		current_user.places.create(place_params)
-		redirect_to root_path
+		@place = current_user.places.create(place_params)
+		if @place.valid?
+			redirect_to root_path
+		else
+			render :new, :status => :unprocessable_entity
+		end
 	end
 	def show
 		@place = Place.find(params[:id])
@@ -22,10 +26,14 @@ class PlacesController < ApplicationController
 	def update
 		@place = Place.find(params[:id])
 		if @place.user != current_user
-			return render :text => 'Not Allowed', :status => forbidden
+			return render :text => 'Not Allowed', :status => :forbidden
 		end
-		@place.update_attributes(place_params)
-		redirect_to root_path
+		if @place.valid?
+			@place.update_attributes(place_params)
+			redirect_to root_path
+		else
+			render :edit, :status => :unprocessable_entity
+		end
 	end
 	def destroy
 		@place = Place.find(params[:id])
