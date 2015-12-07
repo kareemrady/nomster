b6b300a6bf0802a95911b45527b201a0baac288c
Kareem <kareem_rady@yahoo.com>
2015-12-07 00:30:05 +0000

    added email feature

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,3 +1,7 @@
 class NotificationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no-reply@nomsterapp.com"
+
+  def comment_added
+  	mail(to: "kareem_rady@yahoo.com", subject: "A new comment added")
+  end
 end
