5a91f39064c981ac902e6ced894031a94cca5300
Kareem <kareem_rady@yahoo.com>
2015-12-09 01:55:12 +0000

    Added user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Nomster::Application.routes.draw do
   devise_for :users
   root 'places#index'
+  resources :users, :only => :show
   resources :places do
     resources :comments, :only => :create
     resources :photos, :only => :create
