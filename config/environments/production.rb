2ba84c82cb80dd1d173446e82e15d7d44baa50dd
Kareem <kareem_rady@yahoo.com>
2015-12-09 01:02:58 +0000

    configure mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,17 @@ Nomster::Application.configure do
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
+
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
